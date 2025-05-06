209d6bab166ed0b4e88ac1a877a4a8975f20d5f2
aquariusdayroi <lamquangphu2176@gmail.com>
2025-05-07 00:57:37 +0700

    update: model favorite

--- backend/favorite/models.py
+++ backend/favorite/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from user.models import User
 from rental_post.models import RentalPost
+
 # Create your models here.
 class Favorite(models.Model):   
     #ForeignKey
-    user = models.ForeignKey(User,  on_delete=models.CASCADE,null=True, blank=True, related_name='favorite')
-    rentalpost = models.ForeignKey(RentalPost, null=True, blank=True, on_delete=models.CASCADE,  related_name='favorite')
+    user = models.ForeignKey(User,  on_delete=models.CASCADE, related_name='favorite')
+    rentalpost = models.ForeignKey(RentalPost, on_delete=models.CASCADE,  related_name='favorite')
     def __str__(self):
         return f"{self.user}♥{self.rentalpost}"
     
