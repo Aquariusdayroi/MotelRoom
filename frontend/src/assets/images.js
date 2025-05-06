21eeebd134f6897e67de94019c1f4f283497e318
KhangKoii <hpgiakhang.personal@gmail.com>
2025-05-07 03:58:11 +0700

    call list api & handle add/delete favorite

--- frontend/src/assets/images.js
+++ frontend/src/assets/images.js
@@ -1,12 +1,14 @@
-import background from "./img/background.png";
-import logo from "./img/logo.png";
-import Header from "./img/Header.png";
-import notFound from "./img/notFound.png";
+import background from './img/background.png';
+import logo from './img/logo.png';
+import Header from './img/Header.png';
+import notFound from './img/notFound.png';
+import fallbackAvatar from './img/fallback_avatar.png';
 const images = {
     logo,
     background,
     Header,
     notFound,
+    fallbackAvatar,
 };
 
 export { images };
