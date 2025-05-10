56015abf940c1e209b0eb52a48cfa0db6663229b
Thanh <thanhcaovn23@gmail.com>
2025-05-10 14:36:20 +0700

    them API lay thong tin user bang id voi quyen bat ky

--- backend/rental_post/api/urls.py
+++ backend/rental_post/api/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('my-posts/', views.RentalPostListCreateAPIView.as_view(), name='rentalpost-list-create'),
     path('my-posts/<int:id>/', views.RentalPostDetailUpdateDeleteAPIView.as_view(), name='rentalpost-detail-update-delete'),
+    path('my-posts/search/', views.MyRentalPostSearchKeyWordAPIView.as_view(), name='my-rentalpost-search'),
     path('', views.RentalPostListAPIView.as_view(), name= 'rentalpost-list'),
     path('search/', views.RentalPostSearchAPIView.as_view(), name= 'rentalpost-search'),
     # path('my')
