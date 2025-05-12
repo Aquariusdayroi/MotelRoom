2e625f7d89c6fc7e00e25015c9e98eeee41907a5
Thanh <thanhcaovn23@gmail.com>
2025-05-12 16:16:17 +0700

    fix trang owner, field address, them avt vao cmt

--- backend/review/api/serializers.py
+++ backend/review/api/serializers.py
@@ -4,12 +4,14 @@ from user.models import User
 
 class ReviewSerializer(serializers.ModelSerializer):
     avatar = serializers.SerializerMethodField()
+    fullname = serializers.CharField(source='user.fullname', read_only=True)
 
     class Meta:
         model = Review
-        fields = ['id', 'rental_post', 'user', 'avatar', 'rating', 'comment', 'time']
+        fields = ['id', 'rental_post', 'user', 'fullname', 'avatar', 'rating', 'comment', 'time']
         read_only_fields = ['id', 'user', 'time', 'rental_post']
 
     def get_avatar(self, obj):
         # Truy cập avatar của user từ đối tượng Review
         return obj.user.avatar.url if obj.user.avatar else None
+
