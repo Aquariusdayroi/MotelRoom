2cf6bb6c6d6a988e622ae30bae05507bd5a8c568
Khanh <khanhvn3103@gmail.com>
2025-05-26 19:34:13 +0700

    mẻger

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -8,21 +8,21 @@ import AuthProvider from "./authToken";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 const observerErr = window.console.error;
 window.console.error = (...args) => {
-    if (
-        args[0]?.includes?.(
-            "ResizeObserver loop completed with undelivered notifications"
-        )
-    ) {
-        return;
-    }
-    observerErr(...args);
+  if (
+    args[0]?.includes?.(
+      "ResizeObserver loop completed with undelivered notifications"
+    )
+  ) {
+    return;
+  }
+  observerErr(...args);
 };
 root.render(
-    <React.StrictMode>
-        <AuthProvider>
-            <App />
-        </AuthProvider>
-    </React.StrictMode>
+  <React.StrictMode>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
